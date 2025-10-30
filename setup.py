4e8e3eb67e94e543b5d1613cb68ff241dbd00d31
DmitX <154721736+Hieso@users.noreply.github.com>
2025-10-30 10:07:57 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dmitallx',  # Имя вашей библиотеки
+    name='dmitlibx',  # Имя вашей библиотеки
     version='0.1.0',  # Версия
     packages=find_packages(),  # Автоматически находит все пакеты
     install_requires=[
         'rich',  # Укажите зависимости, если они есть
-        'functools',
     ],
     author='DmitX',  # Ваше имя
     author_email='Hieso123@yandex.com',  # Ваш email
     description='Небольшая библиотека для упрощения привычных действий.',  # Краткое описание
     long_description=open('README.md').read(),  # Подробное описание из README
     long_description_content_type='text/markdown',  # Тип содержимого описания
-    url='https://github.com/Hieso/dmitallx',  # Ссылка на репозиторий
+    url='https://github.com/Hieso/dmitlibx',  # Ссылка на репозиторий
     classifiers=[
         'Programming Language :: Python :: 3',  # Поддержка Python 3
         'License :: OSI Approved :: MIT License',  # Лицензия
